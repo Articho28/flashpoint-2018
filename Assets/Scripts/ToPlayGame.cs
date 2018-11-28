cefd4ee6534930988ab0ba0cbec0f834eb69fc40
waelloop <alhashemiw@gmail.com>
2018-11-28 11:41:23 -0500

    Modified Scene and added Scripts

---
+++ Assets/Scripts/ToPlayGame.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ToPlayGame : MonoBehaviour {
+
+    public void switchScenes(){
+        SceneManager.LoadScene("Game", LoadSceneMode.Single);
+    }
+}
