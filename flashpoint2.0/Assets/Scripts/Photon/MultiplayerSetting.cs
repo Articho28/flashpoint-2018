dbda28edf13dc67c678edc10bc9df5a23a8edc87
Articho28 <artsiom.sk@gmail.com>
2019-03-06 09:20:22 -0600

    Modified MultiplayerSetting Script

---
+++ flashpoint2.0/Assets/Scripts/Photon/MultiplayerSetting.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiplayerSetting : MonoBehaviour
+{
+
+    public static MultiplayerSetting MS;
+    public int numOfPlayers;
+    public bool IsFamilyGame;
+
+
+    private void Awake()
+    {
+        if(MultiplayerSetting.MS == null)
+        {
+            MultiplayerSetting.MS = this;
+        }
+        else
+        {
+            if(MultiplayerSetting.MS != this)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void setNumberOfPlayers(int number)
+    {
+        numOfPlayers = number;
+    }
+    public void setFamilyGame(bool isFamilyGame)
+    {
+        this.IsFamilyGame = isFamilyGame;
+    }
+
+
+}
