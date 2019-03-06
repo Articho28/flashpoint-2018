a7566ebaa167a3cbaeda86cc1eefaf42eee09301
Articho28 <artsiom.sk@gmail.com>
2019-03-06 10:39:27 -0600

    Started Photon Lobby script. Connect to server buttons and cancel buttons are funcitonal, but no connection yet

---
+++ flashpoint2.0/Assets/Scripts/Photon/PhotonLobby.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonLobby : MonoBehaviour
+{
+
+
+    public static PhotonLobby lobby;
+
+    public GameObject connectToServerButton;
+    public GameObject connectToServerText;
+    public GameObject cancelButton;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Showing button.");
+        connectToServerText.SetActive(false);
+        connectToServerButton.SetActive(true);
+        cancelButton.SetActive(false);
+    }
+
+    public void OnConnectToServerClicked()
+    {
+        Debug.Log("Connecting to server clicked.");
+        connectToServerButton.SetActive(false);
+        connectToServerText.SetActive(true);
+        cancelButton.SetActive(true);
+    }
+
+    public void OnCancelClicked()
+    {
+        Debug.Log("Cancel Button Clicked");
+        cancelButton.SetActive(false);
+        connectToServerText.SetActive(false);
+        connectToServerButton.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
