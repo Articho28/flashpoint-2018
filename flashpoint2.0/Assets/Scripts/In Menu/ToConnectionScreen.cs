acf56ec3669c96db9d166126485aa80f9a61940e
Articho28 <artsiom.sk@gmail.com>
2019-03-06 10:23:11 -0600

    Added Server Connection Scene and Number of Players Selection parameter passing

---
+++ flashpoint2.0/Assets/Scripts/In Menu/ToConnectionScreen.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToConnectionScreen : MonoBehaviour
+{
+    public int numPlayers;
+
+    public void OnOnePlayerClicked()
+    {
+        numPlayers = 1;
+        switchScenes();
+    }
+    public void OnTwoPlayerClicked()
+    {
+        numPlayers = 2;
+        switchScenes();
+    }
+    public void OnThreePlayerClicked()
+    {
+        numPlayers = 3;
+        switchScenes();
+    }
+    public void OnFourPlayerClicked()
+    {
+        numPlayers = 4;
+        switchScenes();
+    }
+    public void OnFivePlayerClicked()
+    {
+        numPlayers = 5;
+        switchScenes();
+    }
+    public void OnSixPlayerClicked()
+    {
+        numPlayers = 6;
+        switchScenes();
+    }
+
+    public void switchScenes()
+    {
+        MultiplayerSetting.MS.setFamilyGame(true);
+        MultiplayerSetting.MS.setNumberOfPlayers(numPlayers);
+        SceneManager.LoadScene("ServerConnection", LoadSceneMode.Single);
+    }
+}
