4f30fe9a6b527098fdd9d960f57a0bb3a7db3079
Ghida Monla <ghida.monla@mail.mcgill.ca>
2019-03-09 13:04:59 -0500

    added POI, Victim, FalseAlarm, Player

---
+++ flashpoint2.0/Assets/Scripts/In Game/Player.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player
+{
+    PlayerStatus status;
+    bool firstPlayer;
+    Fireman f;
+
+    //constructor
+    public Player(PlayerStatus ps, bool fp, Fireman fireman){
+        this.status = ps;
+        this.firstPlayer = fp;
+        this.f = fireman;
+    }
+
+    public PlayerStatus getStatus()
+    {
+        return this.status;
+    }
+
+    public void setStatus(PlayerStatus newstatus)
+    {
+        this.status = newstatus;
+    }
+
+    public bool getFirstPlayer()
+    {
+        return this.firstPlayer;
+    }
+
+    public void setFP(bool newFP)
+    {
+        this.firstPlayer = newFP;
+    }
+
+    public Fireman getFireman()
+    {
+        return this.f;
+    }
+
+    public void setFireman(Fireman newF)
+    {
+        this.f = newF;
+    }
+
+    public void setFiremanStartingSpace(Space newSpace)
+    {
+        this.f.space = newSpace;
+    }
+
+    public void endTurn()
+    {
+        //TODO
+    }
+
+    /*public WHAT selectKnockDownPlacement()
+    {
+    TODO   
+    }*/
+
+}
