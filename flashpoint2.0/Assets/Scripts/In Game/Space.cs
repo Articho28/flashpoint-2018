f15009c9da98c7c9aa30d15638357b57e4f81a47
waelloop <alhashemiw@gmail.com>
2019-03-09 11:53:27 -0500

    Added attributes

--- flashpoint2.0/Assets/Scripts/In Game/Space.cs
+++ flashpoint2.0/Assets/Scripts/In Game/Space.cs
@@ -1,12 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Space {
     public Vector3 worldPosition;
     public bool isOutside;
     public int indexX;
     public int indexY;
+    Door d1;
+    Wall w1;
 
     public Space(Vector2 _worldPos, bool _isOutside, int _indexX, int _indexY) {
         worldPosition = new Vector3(_worldPos.x, _worldPos.y, 0);
