acc0593b97a8b0520ef79ea732e5b4e92210973e
Andy Jian <jia.jian@mail.mcgill.ca>
2019-03-09 13:28:09 -0500

    added chopwall in Fireman and GameUnit class

--- flashpoint2.0/Assets/Scripts/In Game/Fireman.cs
+++ flashpoint2.0/Assets/Scripts/In Game/Fireman.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Fireman : MonoBehaviour
+public class Fireman : GameUnit
 {
     int actionPoints;
 
     public int getActionPoints() {
         return actionPoints;
+    }
+
+    public void chopWall(Wall wall) {
+        if (wall.addDamage() && actionPoints >= 2) actionPoints -= 2;
     }
 }
---
+++ flashpoint2.0/Assets/Scripts/In Game/GameUnit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameUnit : MonoBehaviour {
+    Space currentSpace;
+
+    public Space getCurrentSpace() {
+        return currentSpace;
+    }
+
+    public void setCurrentSpace(Space space) {
+        currentSpace = space;
+    }
+}
