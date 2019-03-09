6f0aaa06de98bed19f43d47531eb192010b6ab5e
waelloop <alhashemiw@gmail.com>
2019-03-09 11:29:28 -0500

    Created Edge Obstacle objects

---
+++ flashpoint2.0/Assets/Scripts/In Game/Door.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : EdgeObstacleObject
+{
+    DoorStatus status;
+
+    //constructor
+    public Door()
+    {
+        //TODO
+    }
+
+    //door status getter
+    public DoorStatus getDoorStatus()
+    {
+        return this.status;
+    }
+
+    //door status setter
+    public void setDoorStatus(DoorStatus status)
+    {
+        this.status = status;
+    }
+}
---
+++ flashpoint2.0/Assets/Scripts/In Game/EdgeObstacleObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EdgeObstacleObject {
+    Space[] adjacentSpaces;
+
+    //adjacent space getter
+    public Space[] getAdjacentSpaces(){
+        return adjacentSpaces;
+    }
+
+    //adjacent space setter 
+    public void setAdjacentSpaces(Space s1, Space s2){
+        //TODO
+    }
+}
