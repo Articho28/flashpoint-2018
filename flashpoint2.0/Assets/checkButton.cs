73939c70f25713944e9e0f7b2285bbeb77df5152
eidil <elifsu.ates@mail.mcgill.ca>
2019-03-12 21:03:10 -0400

    Tiles added

---
+++ flashpoint2.0/Assets/checkButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class checkButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Button () {
+    	Text t = transform.Find ("Text").GetComponent <Text>();
+    	t.text = "I was pressed";
+
+
+    }
+
+    public void onClick(){
+        Debug.Log("Clicked");
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
