0418db9ea651be1178a9b7e0ac0d81af9950cb57
Articho28 <artsiom.sk@gmail.com>
2019-03-13 17:36:58 -0400

    Working on Network game setup

---
+++ flashpoint2.0/Assets/Scripts/In Game/Network/GamePlayersNetworkSetup.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using System.IO;
+
+public class GamePlayersNetworkSetup : MonoBehaviour
+{
+
+    private static GamePlayersNetworkSetup GS;
+    public string status;
+    public ArrayList players;
+
+    private void OnEnable()
+    {
+        if (GamePlayersNetworkSetup.GS == null)
+        {
+            GamePlayersNetworkSetup.GS = this;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Photon.Realtime.Player p in PhotonNetwork.PlayerList)
+        {
+            Debug.Log("Player " + p.NickName + " is seen in game scene.");
+
+            PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", 
+            "PhotonPlayers", 
+                "PhotonPlayer"),
+            transform.position,
+            Quaternion.identity, 0);
+//            GetComponent<PhotonPlayer>().Initialize(p.NickName);
+        
+        }
+      
+
+
+
+    }
+
+   
+}
---
+++ flashpoint2.0/Assets/Scripts/In Game/Network/PhotonPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class PhotonPlayer : MonoBehaviour
+{
+
+    public string playerName;
+    private PhotonView PV;
+    public GameObject myAvatar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PV = GetComponent<PhotonView>();
+        if (PV.IsMine)
+        {
+            Debug.Log("I see my Photon View.");
+        }
+    }
+
+    public void Initialize(string name)
+    {
+        playerName = name;
+    }
+
+
+}
