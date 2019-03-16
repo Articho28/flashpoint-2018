3ba4918e9856b2085e3365b9f634dd0b5c8a9b5a
waelloop <alhashemiw@gmail.com>
2019-03-16 16:14:24 -0400

    Tried adding fire fighter placement function

--- flashpoint2.0/Assets/Scripts/In Game/Network/FirefighterMovement.cs
+++ flashpoint2.0/Assets/Scripts/In Game/Network/FirefighterMovement.cs
@@ -8,25 +8,37 @@ public class FirefighterMovement : MonoBehaviour
 
     private PhotonView PV;
     private CharacterController myCC;
+    private bool IsFinished;
+    //private GameObject myAvatar;
+
     // Start is called before the first frame update
     void Start()
     {
         PV = GetComponent<PhotonView>();
         myCC = GetComponent<CharacterController>();
+        //myAvatar = GetComponent<PhotonPlayer>().myAvatar;
+        IsFinished = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (PV.IsMine && GameManager.Turn == PhotonNetwork.LocalPlayer.ActorNumber)
+
+        if (PV.IsMine && GameManager.Turn == PhotonNetwork.LocalPlayer.ActorNumber && GameManager.GameStatus == 
+        FlashPointGameConstants.GAME_STATUS_INITIALPLACEMENT && !IsFinished)
         {
-            PlaceFirefighter();
+            IsFinished = PlaceFirefighter();
+            GameManager.IncrementTurn();
         }
 
     }
 
-    public void PlaceFirefighter()
+    public bool PlaceFirefighter()
     {
         //Debug.Log(PhotonNetwork.LocalPlayer.NickName + " is trying to Move! ");
+        Space UserTargetInitialSpace = UserInputManager.instance.getLastSpaceClicked();
+        this.GetComponent<Transform>().transform.position = UserTargetInitialSpace.worldPosition;
+        return true;
+
     }
 }
