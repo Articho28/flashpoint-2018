04cba8e22fe37a426a8ac15f88bf5f03e0491b09
waelloop <alhashemiw@gmail.com>
2019-03-21 19:13:39 -0400

    updated userInputManager

--- flashpoint2.0/Assets/Scripts/In Game/UserInputManager.cs
+++ flashpoint2.0/Assets/Scripts/In Game/UserInputManager.cs
@@ -7,40 +7,46 @@ public class UserInputManager : MonoBehaviour
     #region
     public static UserInputManager instance;
 
-    private void Awake() {
+    private void Awake()
+    {
         instance = this;
     }
 
     #endregion
 
-    GameObject objectClicked;
-    Space spaceClicked;
-    Wall wallClicked;
+    GameObject lastObjectClicked;
+    Space lastSpaceClicked;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) { // if left button pressed
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);             RaycastHit hit;             if (Physics.Raycast(ray, out hit)) {
-                objectClicked = hit.transform.gameObject;
-                 if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Door")) {                     Debug.Log("a door was clicked");                 }                 else if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Wall")) {                     Debug.Log("a wall was clicked");
-                    wallClicked = objectClicked.transform.gameObject.GetComponent<Wall>();                 }                 else if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Board")) {                     Debug.Log("a tile was clicked");
-                    spaceClicked = StateManager.instance.spaceGrid.WorldPointToSpace(objectClicked.transform.position);                 }
+        if (Input.GetMouseButtonDown(0))
+        { // if left button pressed
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);             RaycastHit hit;             if (Physics.Raycast(ray, out hit))
+            {
+                lastObjectClicked = hit.transform.gameObject;
+                 if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Door"))
+                {                     Debug.Log("a door was clicked");                 }                 else if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Wall"))
+                {                     Debug.Log("a wall was clicked");                 }                 else if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Board"))
+                {                     Debug.Log("a tile was clicked");
+                    lastSpaceClicked = StateManager.instance.spaceGrid.WorldPointToSpace(lastObjectClicked.transform.position);                 }
 
             }         }
     }
 
-    public GameObject getObjectClicked() {
-        return objectClicked;
+    public GameObject getLastObjectClicked()
+    {
+        return lastObjectClicked;
     }
 
-    public Space getSpaceClicked() {
-        return spaceClicked;
+    public Space getLastSpaceClicked()
+    {
+        return lastSpaceClicked;
     }
 }
