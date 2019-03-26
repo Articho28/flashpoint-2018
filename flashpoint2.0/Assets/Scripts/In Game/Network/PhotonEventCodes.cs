8a5feccabee8263214db1726064f810db2a596c7
Ghida Monla <ghida.monla@mail.mcgill.ca>
2019-03-25 20:40:55 -0400

    lil changes to fireman

--- flashpoint2.0/Assets/Scripts/In Game/Network/PhotonEventCodes.cs
+++ flashpoint2.0/Assets/Scripts/In Game/Network/PhotonEventCodes.cs
@@ -8,6 +8,8 @@ public enum PhotonEventCodes
     IncrementTurn = 1,
     PlaceInitialFireFighter = 2,
     PlacePOI = 3,
-    PlaceFireMarker = 4
+    PlaceFireMarker = 4,
+    Move = 5,
+    Door= 6
 
 }
